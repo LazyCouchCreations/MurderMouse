adf70e09e9268af37d8791c2ddab75b998683ae6
Epiin <george.seerden@gmail.com>
2018-04-28 01:47:10 -0400

    first upload

---
+++ MurderMouse/Assets/Scripts/MouseContoller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MouseContoller : MonoBehaviour {
+
+    public GameObject mouseAnchorContainer;
+    public List<Transform> mouseAnchors;
+    public NavMeshAgent agent;
+
+    public float anchorWaitTime = 1f;
+
+    private void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        //agent.updateRotation = false;
+
+        foreach (Transform tr in mouseAnchorContainer.transform)
+        {
+            mouseAnchors.Add(tr);
+        }
+
+        StartCoroutine(FindNewAnchor());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator FindNewAnchor()
+    {
+        agent.SetDestination(mouseAnchors[Random.Range(0, mouseAnchors.Count)].position);
+        yield return new WaitForSeconds(anchorWaitTime);
+        StartCoroutine(FindNewAnchor());
+    }
+}
---
+++ MurderMouse/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour {
+
+    public GameObject playerAnchorContainer;
+    public List<Transform> playerAnchors;
+    public NavMeshAgent agent;
+
+    private KeyCode[] keyCodes = { KeyCode.Alpha1, KeyCode.Alpha2, KeyCode.Alpha3, KeyCode.Alpha4, KeyCode.Alpha5, KeyCode.Alpha6, KeyCode.Alpha7, KeyCode.Alpha8 };
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+        //agent.updateRotation = false;
+
+        foreach (Transform tr in playerAnchorContainer.transform)
+        {
+            playerAnchors.Add(tr);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        for(int i = 0; i < keyCodes.Length; i++)
+        {
+            if (Input.GetKeyDown(keyCodes[i]))
+            {
+                try
+                {
+                    MovePlayer(playerAnchors[i]);
+                }
+                catch
+                {
+                    Debug.Log(keyCodes[i].ToString() + " not valid");
+                }
+            }
+        }
+	}
+
+    void MovePlayer(Transform dest)
+    {
+        agent.SetDestination(dest.position);
+    }
+}
